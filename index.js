db021622456ea3b267ab21dd358ce4b4aca91307
Jovanay <jovanay.d.carter@gmail.com>
2021-12-24 00:24:22 -0500

    commit tree df65dd82de448389080fa92b2207fd807f959a65

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let cost;
+  if (distance <= 400){
+    cost = 'This one is on me!'; 
+  } else if (distance > 400 && distance < 2000){
+    cost = 10;
+  } else if (distance >= 2000 && distance <= 2500){
+    cost = 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500) {
+    return 'No can do.'
+  }
+  return cost;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let answer;
+  if (city === 'NYC'){
+    answer = 'Ok, sounds good.';
+  } else {
+    answer = 'No go.';
+  }
+  return answer;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  let charm;
+  if (tip === 'generous'){
+    charm = 'Thank you so much.';
+  } else if (tip === 'not as generous'){
+    charm = 'Thank you.';
+  } else {
+    charm = 'Bye.';
+  }
+  return charm;
+};
